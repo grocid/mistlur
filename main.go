933f969438911a3f1e39ce599200a146188d33ba
Carl Löndahl <carl@Karthago.lan>
2018-02-05 20:56:34 +0100

    fixed a lot of stuffs

--- main.go
+++ main.go
@@ -1,42 +1,70 @@
 package main
 
-import "github.com/murlokswarm/app"
+import (
+    "github.com/faiface/beep"
+    "github.com/faiface/beep/mp3"
+    "github.com/murlokswarm/app"
+    _ "github.com/murlokswarm/mac"
+    "log"
+    "os"
+    "time"
+)
 
 var (
-	win app.Windower
+    win app.Windower
 )
 
 func main() {
-	app.OnLaunch = func() {
-		if menuBar, ok := app.MenuBar(); ok {
-			menuBar.Mount(&MenuBar{})
-		}
+    log.Println(os.Args)
 
-		win = newMainWindow()
-		win.Mount(&Player{})
-	}
+    app.OnLaunch = func() {
+        win = newMainWindow()
+        win.Mount(&Player{})
+    }
 
-	app.OnReopen = func() {
-		if win != nil {
-			return
-		}
-		win = newMainWindow()
-		win.Mount(&Player{})
-	}
+    app.OnReopen = func() {
+        if win != nil {
+            return
+        }
+        win = newMainWindow()
+        win.Mount(&Player{})
+    }
 
-	app.Run()
+    go func() {
+        for _, file := range os.Args[1:] {
+
+            f, err := os.Open(file)
+
+            if err != nil {
+                continue
+            }
+
+            s, format, err := mp3.Decode(f)
+
+            if err != nil {
+                continue
+            }
+
+            Init(format.SampleRate, format.SampleRate.N(time.Second/10))
+            Play(beep.Seq(s, beep.Callback(func() {
+                close(done)
+            })))
+        }
+    }()
+
+    app.Run()
 }
 
 func newMainWindow() app.Windower {
-	return app.NewWindow(app.Window{
-		Title:           "nav",
-		TitlebarHidden:  true,
-		Width:           1280,
-		Height:          768,
-		BackgroundColor: "#21252b",
-		OnClose: func() bool {
-			win = nil
-			return true
-		},
-	})
+    return app.NewWindow(app.Window{
+        Title:          "mistlur",
+        TitlebarHidden: true,
+        Width:          500,
+        Height:         768,
+        Vibrancy:       app.VibeDark,
+        OnClose: func() bool {
+            win = nil
+            return true
+        },
+    })
 }
---
+++ play.go
@@ -0,0 +1,121 @@
+// Based on the internals of https://github.com/faiface/beep
+
+package main
+
+import (
+    "github.com/faiface/beep"
+    "github.com/hajimehoshi/oto"
+    "github.com/ktye/fft"
+    "github.com/pkg/errors"
+    //"os"
+    "sync"
+)
+
+var (
+    mu       sync.Mutex
+    mixer    beep.Mixer
+    samples  [][2]float64
+    csamples []complex128
+    buf      []byte
+    player   *oto.Player
+    underrun func()
+    done     chan struct{}
+    fftc     fft.FFT
+)
+
+const (
+    FFTSamples = 1024
+)
+
+func Init(sampleRate beep.SampleRate, bufferSize int) error {
+    mu.Lock()
+    defer mu.Unlock()
+
+    fftc, _ = fft.New(FFTSamples)
+
+    if player != nil {
+        done <- struct{}{}
+        player.Close()
+    }
+
+    mixer = beep.Mixer{}
+
+    numBytes := bufferSize * 4
+    samples = make([][2]float64, bufferSize)
+    csamples = make([]complex128, FFTSamples)
+    buf = make([]byte, numBytes)
+
+    var err error
+    player, err = oto.NewPlayer(int(sampleRate), 2, 2, numBytes)
+
+    if err != nil {
+        return errors.Wrap(err, "failed to initialize speaker")
+    }
+
+    if underrun != nil {
+        player.SetUnderrunCallback(underrun)
+    }
+
+    done = make(chan struct{})
+
+    go func() {
+        for {
+            select {
+            default:
+                update()
+            case <-done:
+                return
+            }
+        }
+    }()
+
+    return nil
+}
+
+func UnderrunCallback(f func()) {
+    mu.Lock()
+    underrun = f
+    if player != nil {
+        player.SetUnderrunCallback(underrun)
+    }
+    mu.Unlock()
+}
+
+func Lock() {
+    mu.Lock()
+}
+
+func Unlock() {
+    mu.Unlock()
+}
+
+func Play(s ...beep.Streamer) {
+    mu.Lock()
+    mixer.Play(s...)
+    mu.Unlock()
+}
+
+func update() {
+    mu.Lock()
+    mixer.Stream(samples)
+    mu.Unlock()
+
+    for i := range samples {
+        for c := range samples[i] {
+            val := samples[i][c]
+            if val < -1 {
+                val = -1
+            }
+            if val > +1 {
+                val = +1
+            }
+            valInt16 := int16(val * (1<<15 - 1))
+            low := byte(valInt16)
+            high := byte(valInt16 >> 8)
+            buf[i*4+c*2+0] = low
+            buf[i*4+c*2+1] = high
+        }
+    }
+
+    player.Write(buf)
+}
